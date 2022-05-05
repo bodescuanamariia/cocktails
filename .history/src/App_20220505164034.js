9da14dde388e785402daf80c4e50da77c1c5fde6
Bodescu Anamaria <bodescu.anamariia@gmail.com>
2022-05-05 16:46:26 +0300

    feat: route the pages

---
+++ .history/src/App_20220505164034.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
+// import components
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" element={<About />} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
